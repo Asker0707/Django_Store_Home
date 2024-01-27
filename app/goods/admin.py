79539a573c3ee94c77cd4f9891457bb35e90ac01
Asker_samurai <asker_original@mail.ru>
2024-01-27 20:17:12 +0300

    fixtures

--- app/goods/admin.py
+++ app/goods/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from goods.models import Categories, Products
+
+# admin.site.register(Categories)
+# admin.site.register(Products)
+
+#автоматическая проставнока slug
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields =   {'slug': ('name',)}
+    
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields =   {'slug': ('name',)}
